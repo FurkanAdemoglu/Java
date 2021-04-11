b8765e13cf406cafe06fb41c46f8b3bb96e1fa45
Furkan Ademoglu <furkanademoglu69@gmail.com>
2021-04-11 12:47:29 +0300

    Java Data Types Part 3

---
+++ JavaDataTypes3/src/udemyjava3/UdemyJava3.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package udemyjava3;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class UdemyJava3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float myMinFloatValue=Float.MIN_VALUE;
+        float myMaxFloatValue=Float.MAX_VALUE;
+        System.out.println("Float mininmum value="+myMinFloatValue);
+        System.out.println("Float maximum value="+myMaxFloatValue);
+        
+        double myMinDoubleValue=Double.MIN_VALUE;
+        double myMaxDoubleValue=Double.MAX_VALUE;
+        System.out.println("Float mininmum value="+myMinDoubleValue);
+        System.out.println("Float maximum value="+myMaxDoubleValue);
+        //int myIntValue=5;
+        //float myFloatValue=(float)5.25;
+        //double myDoubleValue=5.25d;
+        int myIntValue=5;
+        float myFloatValue=5f;
+        double myDoubleValue=5d;
+        System.out.println("MyIntValue="+myIntValue);
+        System.out.println("MyFloatValue="+myFloatValue);
+        System.out.println("MyDoubleValue="+myDoubleValue);
+        
+        
+        
+        
+        
+    }
+    
+}
